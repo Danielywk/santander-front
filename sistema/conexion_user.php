218a8f812a89452dce901dc6049ec9c67a5bd1af
Danielywk <74077563+Danielywk@users.noreply.github.com>
2023-11-29 21:15:24 -0600

    Delete sistema directory

--- sistema/conexion_user.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion=mysqli_connect("SQL:3306",'root','isaias','base_de_datos') or die ('Error al conectar con la base de datos');
-?>
--- sistema/registros/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion=mysqli_connect("SQL:3306",'root','isaias','system') or die ('Error al conectar con la base de datos');
-?>
