88e128705d493ba9caecdb1ce307f3c61d6b741f
Danielywk <sjilm855@gmail.com>
2023-12-03 04:56:52 +0000

    act

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -5,14 +5,15 @@ session_start();
 $siteName = "Cipet.in";
 
 //DEFINE("BASE_URL","http://cipetbhopal.com/");
-DEFINE("BASE_URL","http://localhost/rainbow/");
+DEFINE("BASE_URL","http://http://54.209.23.44/rainbow/");
 
 DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PSWD', '');
-DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_PSWD', 'isaias');
+DEFINE ('DB_HOST', '54.209.23.44');
 DEFINE ('DB_NAME', 'payroll'); 
 
 date_default_timezone_set('Asia/Calcutta');
 $conn =  new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
 if($conn->connect_error)
 die("Failed to connect database ".$conn->connect_error );
+?>
