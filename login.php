66a4feb561dbac8a5e863b753acf4667dc1cab4c
Danielywk <Danielywk>
2023-12-03 11:07:54 -0600

    ch4

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ $res = $q->fetch_assoc();
 $_SESSION['rainbow_username']=$res['username'];
 $_SESSION['rainbow_uid']=$res['id'];
 $_SESSION['rainbow_name']=$res['name'];
-echo '<script type="text/javascript">window.location="index.php"; </script>';
+echo '<script type="text/javascript">window.location="54.209.23.44/index.php"; </script>';
 
 }else
 {
